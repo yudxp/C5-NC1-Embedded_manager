7c3a43ab9c19d2be247ead5e4eae13c9942ee035
yudxp <yudhahamdi@gmail.com>
2022-04-29 03:33:47 +0700

    Done tampilan

---
+++ Nano_01/DetailPartsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailPartsViewController.swift
+//  Nano_01
+//
+//  Created by Yudha Hamdi Arzi on 29/04/22.
+//
+
+import UIKit
+
+class DetailPartsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Nano_01/ViewController.swift
+++ Nano_01/ViewController.swift
@@ -11,23 +11,26 @@ class ViewController: UIViewController {
 
   @IBOutlet weak var table: UITableView!
   
+  
+  
   struct Component {
     let title : String
     let imageName : String
+    let stockValue : Int
   }
   
   let data: [Component] = [
-    Component(title: "MicroController", imageName: "arduino"),
-    Component(title: "Capacitor", imageName: "capacitor"),
-    Component(title: "Resistor", imageName: "resistor"),
-    Component(title: "Inductor", imageName: "inductor"),
-    Component(title: "Transistor", imageName: "transistor"),
-    Component(title: "Battery", imageName: "battery"),
-    Component(title: "Fuse", imageName: "fuse"),
-    Component(title: "Module", imageName: "module"),
-    Component(title: "Connector", imageName: "connector"),
-    Component(title: "Cable", imageName: "cable"),
-    Component(title: "Led", imageName: "led"),
+    Component(title: "MicroController", imageName: "arduino", stockValue: 5),
+    Component(title: "Capacitor", imageName: "capacitor", stockValue: 2),
+    Component(title: "Resistor", imageName: "resistor", stockValue: 1),
+    Component(title: "Inductor", imageName: "inductor", stockValue: 10),
+    Component(title: "Transistor", imageName: "transistor", stockValue: 99),
+    Component(title: "Battery", imageName: "battery", stockValue: 20),
+    Component(title: "Fuse", imageName: "fuse", stockValue: 10),
+    Component(title: "Module", imageName: "module", stockValue: 25),
+    Component(title: "Connector", imageName: "connector", stockValue: 13),
+    Component(title: "Cable", imageName: "cable", stockValue: 94),
+    Component(title: "Led", imageName: "led", stockValue: 100),
   ]
   
   
@@ -35,13 +38,21 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     table.dataSource = self
     table.delegate = self
-    // Do any additional setup after loading the view.
+    table.separatorStyle = UITableViewCell.SeparatorStyle.none
   }
 
-
+  @IBAction func newParts(_ sender: Any) {
+    
+  }
+  
 }
 
+
 extension ViewController : UITableViewDataSource,UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.cellForRow(at: indexPath)?.isSelected = false
+    performSegue(withIdentifier: "detailPartsSegue", sender: self)
+  }
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return data.count
   }
@@ -49,8 +60,12 @@ extension ViewController : UITableViewDataSource,UITableViewDelegate {
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let component = data[indexPath.row]
     let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
-    cell.label.text = component.title
-    cell.iconImageView.image = UIImage(named: component.imageName)
+    cell.label?.text = " \(component.title) : \(component.stockValue)"
+    cell.iconImageView?.image = UIImage(named: component.imageName)
+    cell.iconImageView.layer.cornerRadius = 20
+    cell.label.layer.masksToBounds = true
+    cell.label.layer.cornerRadius = 5
+//    let padding = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: cell.label.frame.height))
     return cell
   }
   
